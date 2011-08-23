dbfba36b8dd2f4dc4d36b22766262d625f10b01d
Shameer C <shameer@qburst.com>
2011-08-23 15:41:38 +0530

    Some refactoring

--- balls.js
+++ balls.js
@@ -6,26 +6,24 @@
 
 var Balls = function(canvas){
 	
-	// interval in ms
-	var interval 	= 50, ctx, 
+	
+	var ctx, 
 		CELL_SIZE 	= 5,
 		C_WIDTH = 600, C_HEIGHT = 400;
 	// directions
 	var UP = 2, DOWN = 4, LEFT = 8, RIGHT = 16;
 
-	var MAX_X = C_WIDTH / CELL_SIZE;
-	var MAX_Y = C_HEIGHT / CELL_SIZE;
-	
 	var PI    = Math.PI;
 	
-	var angle = PI/4, 
-		radius = 10, 
-		skip = 5
-		vDir = DOWN,
-		hDir = LEFT;
+	var angle 		= PI/4, 
+		interval 	= 40, // interval in ms
+		radius 		= 10, 
+		skip 		= 5
+		vDir 		= DOWN,
+		hDir 		= LEFT;
 
 	// ball position
-	var lastHit= ball = {x:300,y:radius};
+	var lastHit = ball = {x:100,y:radius};
 
 	canvas = $(canvas)[0];
 
@@ -35,18 +33,13 @@ var Balls = function(canvas){
 
 	function startGame(){
 		setInterval(gameLoop,interval);
-		//drawGrid();
-		//drawBall();
-		//clearCanvas();
 	}
 
 	function gameLoop(){
-
 		clearCanvas();
 		drawBall();
 		setDirections();
 		getCoords();
-		//advanceBall();
 	}
 
 	function drawBall(){
@@ -58,11 +51,11 @@ var Balls = function(canvas){
 
 	// set the horizontal and vertical directions on 
 	function setDirections(){
-		if(ball.y >= C_HEIGHT){
+		if(ball.y + radius >= C_HEIGHT){
 			lastHit = {x:ball.x,y:C_HEIGHT};
 			vDir = UP;
 		}
-		else if(ball.y <= 0){
+		else if(ball.y - radius <= 0){
 			lastHit = {x:ball.x,y:0};
 			vDir = DOWN;
 		}
@@ -77,69 +70,43 @@ var Balls = function(canvas){
 
 	}
 
+
+	// function to get the coordinates of the ball
 	function getCoords(){
-		currCords = ball;
 
-		
-		if(vDir == DOWN){
-
-			if(hDir == RIGHT){
-				if(lastHit.y == ball.y){
-					newX = Math.tan(angle) * 1;
-				}
-				else{
-					newX = Math.tan(angle) * (ball.y-lastHit.y);
-				}	
-				ball = { x: lastHit.x+Math.round(newX), y : ball.y+skip};
-			}
-			else{
-				if(lastHit.y == ball.y){
-					newX = Math.tan(angle) * 1;
-				}
-				else{
-					newX = Math.tan(angle) * (ball.y-lastHit.y);
-				}
-				ball = { x: lastHit.x-Math.round(newX), y : ball.y+skip};
-			}
-			
+		// get the absolute value of difference in y coord
+		// to be moved (For finding the x coords to be moved)
+		yAbs    = Math.abs(ball.y-lastHit.y);	
+		// to avoid multiplied by zero error
+		if(yAbs == 0){
+			newX = Math.tan(angle) * 1;
 		}
 		else{
+			newX = Math.round( Math.tan(angle) * yAbs);
+		}
+		// if the ball is moving down
+		// y must be added with the skip value
+		if(vDir == DOWN){
+			// if the direction is right
+			// x must be added
 			if(hDir == RIGHT){
-				if(lastHit.y == ball.y){
-					newX = Math.tan(angle) * 1;
-				}
-				else{
-					newX = Math.tan(angle) * (lastHit.y - ball.y);
-				}
-				ball = { x: lastHit.x+Math.round(newX), y : ball.y-skip};
+				ball = {x: lastHit.x+newX, y : ball.y+skip};
 			}
 			else{
-				if(lastHit.y == ball.y){
-					newX = Math.tan(angle) * 1;
-				}
-				else{
-					newX = Math.tan(angle) * (lastHit.y - ball.y);
-				}
-				//console.log(lastHit.y + ' --- '+ ball.y +'--- newX  ' + newX);
-				ball = { x: lastHit.x-Math.round(newX), y : ball.y-skip};
-				
+				// direction is left, so subtract the x
+				ball = { x: lastHit.x-newX, y : ball.y+skip};
 			}
 		}
-		//console.log(ball.y);
-	}
-
-	function drawGrid(){
-		ctx.strokeStyle = '#cdcdcd';
-		for(var i = 0; i< C_WIDTH; i = i +CELL_SIZE){
-			//console.log(i);
-			ctx.beginPath();
-			ctx.moveTo(i,0);
-			ctx.lineTo(i,C_HEIGHT);
-			ctx.stroke();
-			if(i<C_HEIGHT){
-				ctx.moveTo(0,i);
-				ctx.lineTo(C_WIDTH,i);
-				ctx.stroke();
+		else{
+			// direction is up
+			// subtract skip from y
+			if(hDir == RIGHT){
+				// direction is right, add x
+				ball = { x: lastHit.x+newX, y : ball.y-skip};
+			}
+			else{
+				// direction is left, subtract s
+				ball = { x: lastHit.x-newX, y : ball.y-skip};
 			}
 		}
 	}
@@ -147,11 +114,6 @@ var Balls = function(canvas){
 	function clearCanvas(){
 		ctx.clearRect(0,0,C_WIDTH,C_HEIGHT);
 	}
-
-	function roundNumber(num, dec) {
-		var result = Math.round( num * Math.pow(10,dec)) / Math.pow(10,dec);
-		return result;
-    }
 
 	return {
 		start : startGame
